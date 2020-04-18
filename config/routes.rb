ee216efa1d21882226656d70cc944e66f7daed0a
Elton de Andrade Rodrigues <eltonrodrigues61@gmail.com>
2020-04-18 15:06:43 -0300

    old code

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
-  #get 'cast/new'
-  #get '/cast'  => 'cast#index'
-  #post '/cast/create'
-  #get '/home/index'
-  #delete '/cast/:id(.:format)', :to => 'cast#destroy'
-  #delete "cast/:id" => "cast#destroy", as: :delete
-  
-
-  root 'home#index'
-
-  resources :cast, only: [:create, :show, :index, :destroy]
-  resources :users, only: [:create, :destroy]
-  resources :sessions, only: [:new, :create, :destroy]
-
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
- 
